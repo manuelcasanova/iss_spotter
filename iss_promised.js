5ee14d59ba14dfa39813b2978f77f09da0494214
Manuel Casanova <manucasanova@hotmail.com>
2022-02-25 14:56:42 -0800

    FETCHCOORDSBYIP CODE ADDED BEFORE TOGGLING ANSWER

--- iss_promised.js
+++ iss_promised.js
@@ -6,4 +6,16 @@ const fetchMyIP = function() {                              //Returns a promise 
 
 //console.log(fetchMyIP());
 
-module.exports = { fetchMyIP };
+
+const fetchCoordsByIP = function(body) {  //Makes a request to freegeoip.app Gets latitude/longitude using the provided IP. Returns a promise of request for latitude/longitude
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`)
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
+
+
+module.exports = { 
+  fetchMyIP,
+  fetchCoordsByIP
+};
