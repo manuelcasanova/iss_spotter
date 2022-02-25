2c9ff1fe04b7a9221653a1a95659aaf11865870b
Manuel Casanova <manucasanova@hotmail.com>
2022-02-25 13:17:35 -0800

    CODE FIXED AFTER TOGGLING ANSWER

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIp, fetchIssFlyOverTimes, nextISSTimesForMyLocation} = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchIssFlyOverTimes, nextISSTimesForMyLocation} = require('./iss');
 
 
 // fetchMyIP((error, ip) => {
@@ -12,7 +12,7 @@ const { fetchMyIP, fetchCoordsByIp, fetchIssFlyOverTimes, nextISSTimesForMyLocat
 
 
 
-// fetchCoordsByIp('24.80.197.125', (error, data) => {//Shouldn't I use fetchMyIp function to get the IP instead of writing it as a string
+// fetchCoordsByIP('24.80.197.125', (error, data) => {//Shouldn't I use fetchMyIp function to get the IP instead of writing it as a string
 //   if (error) {
 //     console.log(`It didn't work!`, error);
 //     return;
@@ -32,16 +32,25 @@ const { fetchMyIP, fetchCoordsByIp, fetchIssFlyOverTimes, nextISSTimesForMyLocat
 //   console.log(`It worked! Time and duration`, data);
 // });
 
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
-  // success, print out the deets!
-  console.log(passTimes);
+  printPassTimes(passTimes);
 });
+
+
 
 
 module.exports = {
   fetchMyIP,
-  fetchCoordsByIp };
+  fetchCoordsByIP };
