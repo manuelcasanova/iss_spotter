e3ffae55198c8de727bb63b2636229d3a48625a8
Manuel Casanova <manucasanova@hotmail.com>
2022-02-25 08:57:01 -0800

    COMPLETED AFTER TOGGLING ANSWER

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ fetchCoordsByIp('24.80.197.125', (error, data) => {//Shouldn't I use fetchMyIp f
   console.log(`It worked! Coordinates:`, data);
 });
 
-fetchIssFlyOverTimes('49.2643, -123.0961', (error, data) => {
+const exampleCoords = { latitude: "49.2643", longitude: "-123.0961"};
+
+fetchIssFlyOverTimes(exampleCoords, (error, data) => {
   if (error) {
     console.log(`It didn't work!`, error);
     return;
