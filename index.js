364128dc66674090438d3e558821eaa4f08592d5
Manuel Casanova <manucasanova@hotmail.com>
2022-02-24 19:52:03 -0800

    TESTED AND CALL COMMENTED OUT SINCE VERIFICATION COMPLETED

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ fetchMyIP((error, ip) => {
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  // console.log('It worked! Returned IP:' , ip);
 });
 
 
 
 fetchCoordsByIp('24.80.197.125', (error, data) => {
-console.log(error);
-console.log(data);
+  if (error) {
+    console.log(`It didn't work!`, error);
+    return;
+  }
 
-
+// console.log(`It worked! Coordinates:`, data);
 });
 
 
