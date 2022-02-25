2aade64acf0ef7264e13d27d081f68dba1646d2f
Manuel Casanova <manucasanova@hotmail.com>
2022-02-25 12:25:26 -0800

    NEXTISSTIMESFORMYLOCATION FUNCTION ADDED EMPTY AND TESTED FOR NOR OUTPUT NO ERRORS

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-const { fetchMyIP, fetchCoordsByIp, fetchIssFlyOverTimes } = require('./iss');
+const { fetchMyIP, fetchCoordsByIp, fetchIssFlyOverTimes, nextISSTimesForMyLocation} = require('./iss');
 
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+
+
+// fetchCoordsByIp('24.80.197.125', (error, data) => {//Shouldn't I use fetchMyIp function to get the IP instead of writing it as a string
+//   if (error) {
+//     console.log(`It didn't work!`, error);
+//     return;
+//   }
+
+//   console.log(`It worked! Coordinates:`, data);
+// });
+
+// const exampleCoords = { latitude: "49.2643", longitude: "-123.0961"};
+
+// fetchIssFlyOverTimes(exampleCoords, (error, data) => {
+//   if (error) {
+//     console.log(`It didn't work!`, error);
+//     return;
+//   }
+
+//   console.log(`It worked! Time and duration`, data);
+// });
+
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!" , error);
-    return;
+    return console.log("It didn't work!", error);
   }
-
-  console.log('It worked! Returned IP:' , ip);
+  // success, print out the deets!
+  console.log(passTimes);
 });
-
-
-
-fetchCoordsByIp('24.80.197.125', (error, data) => {//Shouldn't I use fetchMyIp function to get the IP instead of writing it as a string
-  if (error) {
-    console.log(`It didn't work!`, error);
-    return;
-  }
-
-  console.log(`It worked! Coordinates:`, data);
-});
-
-const exampleCoords = { latitude: "49.2643", longitude: "-123.0961"};
-
-fetchIssFlyOverTimes(exampleCoords, (error, data) => {
-  if (error) {
-    console.log(`It didn't work!`, error);
-    return;
-  }
-
-  console.log(`It worked! Time and duration`, data);
-});
-
 
 
 module.exports = {
